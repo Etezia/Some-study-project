b9277f145906828072656352c7e239797fa6f0dd
Etezia <maksroll9@gmail.com>
2026-09-24 23:46:07 +0500

    task completed in 2 ways

--- Assets/Scripts/CameraManipulator.cs
+++ Assets/Scripts/CameraManipulator.cs
@@ -1,17 +1,15 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class CameraManipulator : MonoBehaviour
 {
     [SerializeField] private Transform _cameraTransform;
 
-	private const float MinCamDistance = 15f;
-	private const float MaxCamDistance = 50f;
+	private const float MinCamDistance = 10f;
+	private const float MaxCamDistance = 70f;
 
     private InputSystem_Actions _inputSystem;
-	private const float MoveSpeed = 24f;
-	private const float ZoomSpeed = 32f;
+	private const float MoveSpeed = 28f;
+	private const float ZoomSpeed = 40f;
 
 	private void Awake() => _inputSystem = new InputSystem_Actions();
 
@@ -25,14 +23,10 @@ public class CameraManipulator : MonoBehaviour
 		transform.Rotate(transform.up, -moveInputScaled.x);
 		_cameraTransform.RotateAround(transform.position, transform.right, moveInputScaled.y);
 
-		// --- Old variant ---
 		var distance = (_cameraTransform.position - transform.position).magnitude;
 		if (zoomInputScaled > 0 && distance > MinCamDistance
 			|| zoomInputScaled < 0 && distance < MaxCamDistance)
-			_cameraTransform.Translate(new Vector3(0f, 0f, zoomInputScaled));
-
-		//_cameraTransform.localPosition = new Vector3(_cameraTransform.localPosition.x, _cameraTransform.localPosition.y,
-		//	Math.Clamp(_cameraTransform.localPosition.z + zoomInputScaled, -MaxCamDistance, -MinCamDistance));
+			_cameraTransform.Translate(new Vector3(0f, 0f, zoomInputScaled));	//idk, maybe there's better way to clamp zoom
 	}
 
 	private void OnDisable() => _inputSystem.Camera.Disable();
